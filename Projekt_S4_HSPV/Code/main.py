8b974faf8d5c948d37c3588f7e3f9085af6ba7bf
Tim <tim.terbrack@gmail.com>
2024-03-22 16:32:53 +0100

    ⏰ = 🏁 Canny, Blurred, Lokale Binarisierung. Alles nur Testweise. Todo: Auslagern u.v.m.

--- Projekt_S4_HSPV/Code/main.py
+++ Projekt_S4_HSPV/Code/main.py
@@ -46,9 +46,13 @@ def create_original_image(image):
 
 
 def create_binary_image(image):
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, image_binary = cv2.threshold(image_gray, 127, 255, cv2.THRESH_BINARY)
-    image_binary_dictionary = {"title": "Binary Image", "image": image_binary}
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_blurred = cv2.GaussianBlur(image, (55, 55), 0)
+    image_gray = cv2.cvtColor(image_blurred, cv2.COLOR_BGR2GRAY)
+    #_, image_binary = cv2.threshold(image_gray, 127, 255, cv2.THRESH_BINARY)
+    image_binary = cv2.adaptiveThreshold(image_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 0.5)
+    image_canny = cv2.Canny(image_binary, 100, 200)
+    image_binary_dictionary = {"title": "Binary Image", "image": image_canny}
     return image_binary_dictionary
 
 
@@ -58,9 +62,14 @@ def create_blurred_image(image):
     image_blurred_dictionary = {"title": "Blurred Image", "image": image_blurred}
     return image_blurred_dictionary
 
+
+blurred_image = create_blurred_image(image_BGR)
+blurred_image = blurred_image["image"]
+blurred_image = cv2.cvtColor(blurred_image, cv2.COLOR_RGB2BGR)
+
 image_array = []
 image_array.append(create_original_image(image_BGR))
-image_array.append(create_binary_image(image_BGR))
+image_array.append(create_binary_image(blurred_image))
 image_array.append(create_blurred_image(image_BGR))
 
 plot_images(image_array)
