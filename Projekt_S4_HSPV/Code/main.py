37326747e69e059cc0187518c39a5a43a1ffce01
Tim <tim.terbrack@gmail.com>
2024-04-05 16:28:17 +0200

    Erstes funktionales Portraitbild Merging und Dilatation der Maske mit dem Originalbild hinzugefügt.

--- Projekt_S4_HSPV/Code/main.py
+++ Projekt_S4_HSPV/Code/main.py
@@ -5,7 +5,7 @@ from pygments.formatters import img
 
 path = "../Images/Thermobecher.jpg"
 image_BGR = cv2.imread(path)
-image_RGB = cv2.cvtColor(image_BGR, cv2.COLOR_BGR2RGB) #Simon sagt, er möchte es noch behalten.
+image_RGB = cv2.cvtColor(image_BGR, cv2.COLOR_BGR2RGB)
 
 # Methode um Bilder darzustellen
 
@@ -28,7 +28,7 @@ def plot_images(img_arr):
         plot_single_image(img_arr[0])
         return
 
-    fig, ax = plt.subplots(1, len(img_arr), sharex=False, sharey=False)
+    fig, ax = plt.subplots(1, len(img_arr), sharex=True, sharey=True)
     
     for i in range(0, len(img_arr)):
         ax[i].axis("off")
@@ -45,67 +45,53 @@ def create_original_image(image):
 
     return original_image_dictionary
 
-
-def create_binary_image(image):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image_blurred = cv2.GaussianBlur(image, (55, 55), 0)
-    image_gray = cv2.cvtColor(image_blurred, cv2.COLOR_BGR2GRAY)
-    #_, image_binary = cv2.threshold(image_gray, 127, 255, cv2.THRESH_BINARY)
-    image_binary = cv2.adaptiveThreshold(image_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 0.5)
-    #image_canny = cv2.Canny(image_binary, 100, 200)
-    image_binary_dictionary = {"title": "Binary Image", "image": image_binary}
-    return image_binary_dictionary
-
-def create_canny_image(image):
-        image_canny = cv2.Canny(image, 100, 200.0)
-        image_canny_dictionary = {"title": "Canny Image", "image": image_canny}
-        return image_canny_dictionary
-
-
-
 def create_image_blurred(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image_blurred = cv2.GaussianBlur(image, (55, 55), 0)
+    image_blurred = cv2.GaussianBlur(image, (111, 111
+                                             ), 0)
     image_blurred_dictionary = {"title": "Blurred Image", "image": image_blurred}
     return image_blurred_dictionary
 
-def create_cutted_image(image):
-    x1, y1 = 830, 1057
-    x2, y2 = 2069, 3619
-    image_cutted = image[y1:y2, x1:x2]
-    return image_cutted
 
-def grabcut_image(img):
-    mask = np.zeros(img.shape[:2], np.uint8)
+def grabcut_image(image):
+    mask = np.zeros(image.shape[:2], np.uint8)
     bgdModel = np.zeros((1, 65), np.float64)
     fgdModel = np.zeros((1, 65), np.float64)
     rect = (830, 1057, 1200, 2500)
-    cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+    cv2.grabCut(image, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
     mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
-    img = img * mask2[:, :, np.newaxis]
-    plt.imshow(img), plt.colorbar(), plt.show()
+    return {"title": "Grabcut Maske", "image" : mask2}
 
 
-grabcut_image(image_RGB)
-# Bild zuschneiden auf den relevanten Bereich
-image_cutted = create_cutted_image(image_BGR)
+def image_merge(image_original, image_blurred, mask):
+    image_merged = image_blurred.copy()
+    for x in range (image_original.shape[0]):
+        for y in range (image_original.shape[1]):
+            if mask[x][y] == 1:
+                image_merged[x][y] = image_original[x][y]
+    image_merged_dictionary = {"title": "Merged Image", "image" : image_merged}
+    return image_merged_dictionary
+
+def create_dilated_mask(mask):
+    dilated_mask = mask.copy()
+    dilated_mask = cv2.dilate(dilated_mask, kernel=np.ones((5, 5), np.uint8), iterations=3)
+    image_dilated_dictionary = {"title": "Dilated Image", "image": dilated_mask}
+    return image_dilated_dictionary
 
 
-image_blurred = create_image_blurred(image_cutted)
+
+image_blurred = create_image_blurred(image_BGR)
 image_blurred = image_blurred["image"]
-image_blurred = cv2.cvtColor(image_blurred, cv2.COLOR_RGB2BGR)
-image_binary = create_binary_image(image_blurred)
-image_binary = image_binary["image"]
-
-
-
-
+image_masked = grabcut_image(image_RGB)
+image_masked = image_masked["image"]
+image_dilated = create_dilated_mask(image_masked)
+image_dilated = image_dilated["image"]
 
 
 image_array = []
 image_array.append(create_original_image(image_BGR))
-image_array.append(create_binary_image(image_blurred))
-image_array.append(create_image_blurred(image_BGR))
-image_array.append(create_canny_image(image_binary))
+image_array.append(grabcut_image(image_RGB))
+image_array.append(create_dilated_mask(image_masked))
+image_array.append(image_merge(image_RGB,image_blurred,image_dilated))
 
 plot_images(image_array)
